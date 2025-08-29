77796051eaeb55788c275262c36df15309683b4d
Avie Vasantlal <avasantlal@gmail.com>
2025-08-29 14:07:36 -0400

    Properly set up SvelteKit to make the frontend easier and adaptable

--- frontend-svelte/src/lib/api.js
+++
@@ -1,8 +0,0 @@
-export async function fetchPortfolio() {
-  const res = await fetch('http://localhost:8080/api/portfolio');
-  if (!res.ok) {
-    throw new Error('Network response was not ok');
-  }
-  const data = await res.json();
-  return data.projects || [];
-}
