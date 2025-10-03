9ef9e6b406fb9a6d1fcba1e95b3d9af5ebfa9a51
Avie Vasantlal <avasantlal@gmail.com>
2025-10-02 22:12:36 -0400

    Remod;

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -2,9 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import PortfolioTemplate from './components/PortfolioTemplate';
+import React from 'react'
 
 function App() {
   const [count, setCount] = useState(0)
+  const App = () => {
+  return <PortfolioTemplate />;
+};
 
   return (
     <>
